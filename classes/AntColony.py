ae2df07a24d4024215d9eb730297353fb8be41e9
David <david.nunez@oktana.io>
2021-11-01 17:25:17 -0300

    Cambio de M3AS to MAS

--- classes/AntColony.py
+++ classes/AntColony.py
@@ -1,15 +1,16 @@
 from numpy.random import choice as np_choice
 import numpy as np
+from numpy.random.mtrand import pareto
+import copy
 
 class AntColony(object):
-    def __init__(self, distances, n_ants, n_best, n_iterations, decay, clients, capacity, alpha=1, betha=1):
+    def __init__(self, distances, n_ants, n_iterations, decay, clients, capacity, alpha=1, betha=1, k_prima=10):
         self.distances = distances
         self.pheromone = np.ones(self.distances.shape)/ len(distances)
         # vis_i,j = 1/ d_i,j
         self.visibility = [ [ 1/distance for distance in row] for row in self.distances ]
         self.all_inds = range(len(distances))
         self.n_ants = n_ants
-        self.n_best = n_best
         self.n_iterations = n_iterations
         self.decay = decay
         self.alpha = alpha
@@ -17,14 +18,18 @@ class AntColony(object):
         self.clients = clients
         self.capacity = capacity
         self.pareto = []
+        self.k_prima = k_prima
+    
+    def restart_pheromones(self):
+        self.pheromone = np.ones(self.distances.shape)/ len(self.distances)
 
     def get_p_table(self, position_index, visited, cargo, time):
         prob = np.copy( self.pheromone[position_index] )
         prob[list(visited)] = 0 #Si ya visite no hace falta volver
         current_client = self.clients[position_index]
         for i in range( len(prob) ):
-            next_client = self.clients[ i ]
             if prob[i] != 0:
+                next_client = self.clients[ i ]
                 next_time = time + current_client.get_distance_to_client(next_client)
                 if cargo + next_client.demand > self.capacity:
                     #Si la sgte demanda + carga_actual > capacidad, no puedo cumplir el destino
@@ -36,17 +41,14 @@ class AntColony(object):
         for i in range( len(prob) ):
             if prob[i] != 0:  #Si es parte del vecindario
                 # SUMATORIA de Tao_i,g * (n_i,g ^ alpha) ^ betha
-                den_expr += (prob[i] * (self.visibility[position_index][i] ** self.alpha) ** self.betha) 
+                den_expr += prob[i] * (self.visibility[position_index][i] ** (self.alpha * self.betha))
 
         for i in range( len(prob) ):
             if prob[i] != 0:  #Si es parte del vecindario
                 # p_i,j = (Tao_i,j * (n_i,j ^ alpha) ^ betha) / 
-                prob[i] = (( prob[i] * self.visibility[position_index][i] ** self.alpha ) ** self.betha) / den_expr
-        
-        # if prob.sum() != 0:
-        #     prob = prob / prob.sum()
+                prob[i] = ( prob[i] * self.visibility[position_index][i] ** (self.alpha * self.betha) ) / den_expr
 
-        return prob 
+        return prob / prob.sum() if prob.sum() != 0 else prob
 
     def gen_path(self, start):
         path = []
@@ -77,56 +79,87 @@ class AntColony(object):
         time = 0.0
         vehicles = 0
         for route in path:
-            cost += self.distances[route]
             if route[0] == 0: #If we start over it means is a new vehicle
                 vehicles += 1
+                cost += time
                 time = 0.0
             time += self.distances[route]
             destiny = self.clients[ route[1] ]
             time = destiny.get_time_after_service(time)
+            if time == 0: 
+                print('im 0')
+        cost += time
+        if cost == 0:
+            print('im 0')
         return cost, vehicles
+
+    def does_solution_dominates(self, solution_a, solution_b):
+        return ( solution_a[1] <= solution_b[1] and solution_a[2] <= solution_b[1] ) and (
+            solution_a[1] < solution_b[1] or solution_a[2] < solution_b[1]
+        )
+
+    def update_pareto(self, pareto_set, path, cost, vehicles):
+        new_solution = (path, cost, vehicles)
+        new_pareto = []
+        if len(pareto_set) == 0:
+            new_pareto.append( new_solution )
+        else:
+            new_solution_is_dominated = False
+            #Actualiza pareto con las soluciones no dominadas por la solucion nueva
+            new_pareto = [ sol for sol in pareto_set if not self.does_solution_dominates(new_solution, sol) ]
+            for sol in new_pareto:
+                if self.does_solution_dominates(sol, new_solution):
+                    new_solution_is_dominated = True #Verifica si una solucion del pareto domina a la nueva solucion
+                    break
+            if not new_solution_is_dominated:# Si ninguna solucion del pareto domina a la nueva solucion se agrega al pareto
+                new_pareto.append(new_solution)
+        return new_pareto
 
     def gen_all_paths(self):
         all_paths = []
+        local_pareto = []
         for i in range(self.n_ants):
             path = self.gen_path(0)
             cost, vehicles = self.get_path_costs(path)
+            local_pareto = self.update_pareto( local_pareto, path, cost, vehicles )
+            # has_to_update_pheromones = self.update_pareto( path, cost, vehicles )
+            # self.spread_pheromone( not has_to_update_pheromones )
             all_paths.append( ( path, cost, vehicles ) )
-        return all_paths
+        return all_paths, local_pareto
     
-    def spread_pheromone(self, all_paths, n_best):
-        ##Primero evaporar:
-        for i in range(len(self.pheromone) - 1):
-            for j in range(len(self.pheromone[0]) - 1):
-                # Tao_i,j = (1-p) Tao_i,j + p * T_0
-                #T_0 = self.distances[i][j]/ len(self.distances)
-                self.pheromone[i][j] = (1 - self.decay) * self.pheromone[i][j] + self.decay * (self.distances[i][j]/ len(self.distances))
-        ##Luego de evaporar, actualizar
-        sorted_paths = sorted(all_paths, key=lambda x: x[1])
-        for path, dist, vehicles in sorted_paths[:n_best]:
-            delta_tao = 1.0 / (dist * vehicles)
+    def spread_pheromone(self, updating_ants, max_distance, max_vehicles):
+        #Evaporacion
+        for i in range( len(self.pheromone) -1 ):
+            for j in range( len(self.pheromone[0]) -1 ):
+                self.pheromone[i][j] = self.pheromone[i][j] * (1 - self.decay)
+
+        #Actualizacion
+        for path, dist, vehicles in updating_ants:
+            delta_tao = 1.0 / ( (dist/max_distance) + (vehicles/max_vehicles) ) #Normalization
             for move in path:
-                new_tao_ij = self.pheromone[move] + delta_tao
-                t_min = delta_tao / ( 1 - self.decay )
-                t_max = delta_tao / ( 2 * self.n_ants * (1 - self.decay) )
-                if new_tao_ij >= t_max:
-                    self.pheromone[move] = t_max
-                elif new_tao_ij <= t_min:
-                    self.pheromone[move] = t_min
-                else:
-                    self.pheromone[move] = new_tao_ij
+                self.pheromone[move] = self.pheromone[move] +  self.decay * delta_tao
 
     def run(self):
         distance_logs = []
         shortest_path = None
         all_time_shortest_path = ("placeholder", np.inf)
+        k_index = 0
+        old_pareto = copy.deepcopy(self.pareto)
         for i in range(self.n_iterations):
-            all_paths = self.gen_all_paths()
-            #Actualizar el conjunto pareto
-            self.spread_pheromone(all_paths, self.n_best)
-            shortest_path = min(all_paths, key=lambda x: x[1])
-            print('Generation ', i, ' Best cost', shortest_path[1], 'Vehicles ', shortest_path[2])
+            all_paths, local_pareto = self.gen_all_paths()
+            shortest_path = min( all_paths, key=lambda x: x[1] )
+            max_vehicles = max( all_paths, key=lambda x: x[2] )[2]
+            max_distance = max( all_paths, key=lambda x: x[1] )[1]
+            self.spread_pheromone(local_pareto, max_distance, max_vehicles)
+            print ("{:<8} {:<20} {:<10} {:<20} {:<10}".format(i, shortest_path[1], shortest_path[2], all_time_shortest_path[1], len(self.pareto)))
             if shortest_path[1] < all_time_shortest_path[1]:
                 all_time_shortest_path = shortest_path
-            distance_logs.append(all_time_shortest_path[1])                      
+            distance_logs.append(all_time_shortest_path[1]) 
+            if old_pareto == self.pareto:
+                k_index += 1
+            else:
+                k_index = 0
+                old_pareto = copy.deepcopy(self.pareto)
+            if k_index == 10:
+                self.restart_pheromones();
         return all_time_shortest_path,distance_logs
--- masScript.py
+++ masScript.py
@@ -7,8 +7,8 @@ from utils import get_adjacency_matrix, read_file
 def main():
     data, capacity = read_file("vrptw_c101.txt", 1000)
     distances = np.array(get_adjacency_matrix(data))
-    print(distances)
-    ant_colony = AntColony(distances, 100, 50, 30, 0.6, data, capacity, alpha=1, betha=1)
+    ant_colony = AntColony(distances, 100, 30, 0.7, data, capacity, alpha=1, betha=1.5)
+    print ("{:<8} {:<20} {:<10} {:<20} {:<10}".format("Gen #", "Best it ant", "Vehicles", "Best ovrll ant", "Pareto len"))
     shortest_path,log = ant_colony.run()
     print ("shortest_path: {}".format(shortest_path))
     plt.plot(log)
